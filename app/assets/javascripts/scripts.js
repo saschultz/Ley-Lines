7d0fcb8f3cad1ae15a8f13136de2e8a09382de55
Sara Schultz <saschultz8@gmail.com>
2017-07-25 15:48:29 -0700

    add lat and long for Portland

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,8 +1,8 @@
 var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: 45.5231, lng: -122.6765},
+    zoom: 12
   });
 }
 
