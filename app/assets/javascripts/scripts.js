71fbbe2c27851a71c7158330914ef3ea59d8843d
Guest User <Guest@Epicodus-2B.local>
2017-07-26 20:22:52 -0700

    will this ignore my secrets?

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,11 +1,84 @@
-var map;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 45.5231, lng: -122.6765},
-    zoom: 12
-  });
-}
 
+function initMap() {
+  var markerArray = [];
+
+  var directionsService = new google.maps.DirectionsService;
+
+
+  map = new google.maps.Map(document.getElementById('map'), {
+  center: {lat: 45.5231, lng: -122.6765},
+  zoom: 12
+  });
+
+  // Create a renderer for directions and bind it to the map.
+          var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+
+          // Instantiate an info window to hold step text.
+          var stepDisplay = new google.maps.InfoWindow;
+
+          // Display the route between the initial start and end selections.
+          calculateAndDisplayRoute(
+              directionsDisplay, directionsService, markerArray, stepDisplay, map);
+          // Listen to change events from the start and end lists.
+          var onChangeHandler = function() {
+            calculateAndDisplayRoute(
+                directionsDisplay, directionsService, markerArray, stepDisplay, map);
+          };
+          document.getElementById('start').addEventListener('change', onChangeHandler);
+          document.getElementById('end').addEventListener('change', onChangeHandler);
+        }
+
+        function calculateAndDisplayRoute(directionsDisplay, directionsService,
+            markerArray, stepDisplay, map) {
+          // First, remove any existing markers from the map.
+          for (var i = 0; i < markerArray.length; i++) {
+            markerArray[i].setMap(null);
+          }
+
+          // Retrieve the start and end locations and create a DirectionsRequest using
+          // WALKING directions.
+          directionsService.route({
+            origin: document.getElementById('start').value,
+            destination: document.getElementById('end').value,
+            travelMode: 'DRIVING'
+          }, function(response, status) {
+            // Route the directions and pass the response to a function to create
+            // markers for each step.
+            if (status === 'OK') {
+              document.getElementById('warnings-panel').innerHTML =
+                  '<b>' + response.routes[0].warnings + '</b>';
+              directionsDisplay.setDirections(response);
+              showSteps(response, markerArray, stepDisplay, map);
+            } else {
+              window.alert('Directions request failed due to ' + status);
+            }
+          });
+        }
+
+        function showSteps(directionResult, markerArray, stepDisplay, map) {
+          // For each step, place a marker, and add the text to the marker's infowindow.
+          // Also attach the marker to an array so we can keep track of it and remove it
+          // when calculating new routes.
+          var myRoute = directionResult.routes[0].legs[0];
+          for (var i = 0; i < myRoute.steps.length; i++) {
+            var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+            marker.setMap(map);
+            marker.setPosition(myRoute.steps[i].start_location);
+            attachInstructionText(
+                stepDisplay, marker, myRoute.steps[i].instructions, map);
+          }
+        }
+
+        function attachInstructionText(stepDisplay, marker, text, map) {
+          google.maps.event.addListener(marker, 'click', function() {
+            // Open an info window when the marker is clicked on, containing the text
+            // of the step.
+            stepDisplay.setContent(text);
+            stepDisplay.open(map, marker);
+          });
+        }
+
+///////FRONT END///////
 $(document).ready(function() {
   initMap();
 });
